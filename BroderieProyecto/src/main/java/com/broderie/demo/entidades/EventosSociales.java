dfb41511b80c54661d7cb1fa1fadaeb4f39279b9
Luis Gutierrez <luisoguti09@gmail.com>
2021-12-21 19:10:20 -0300

    netBeans

---
+++ BroderieProyecto/src/main/java/com/broderie/demo/entidades/EventosSociales.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.broderie.demo.entidades;
+
+/**
+ *
+ * @author Luis
+ */
+public class EventosSociales {
+    
+}
